3f64a9fdf2847e1ac29b9f0c49b8d1685eb57807
Patrick Her <renfu.her@gmail.com>
2025-07-29 11:01:11 +0800

    更新待辦事項資源，將描述欄位的編輯器更改為 Markdown 編輯器，並新增說明文字以指導使用者使用 Markdown 語法。

--- app/Filament/Resources/TodoResource.php
+++ app/Filament/Resources/TodoResource.php
@@ -41,8 +41,9 @@ class TodoResource extends Resource
                             ->maxLength(255)
                             ->columnSpanFull(),
 
-                        TinyEditor::make('description')
+                        Forms\Components\MarkdownEditor::make('description')
                             ->label('描述')
+                            ->helperText('使用 Markdown 語法撰寫')
                             ->columnSpanFull(),
 
                         Forms\Components\Select::make('project_id')
